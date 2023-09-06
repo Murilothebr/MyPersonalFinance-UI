edeb09344f36cd65b5f4ed480dfd5dfdd88f1100
murilothebr <murilothebr@hotmail.com>
2023-09-06 18:33:46 -0300

    before clone

--- MyPersonalFinance/src/app/app-routing.module.ts
+++ MyPersonalFinance/src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
+import { AuthGuard } from './helpers/auth.guard';
 import { SecretComponent } from './secret/secret.component';
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
     path: '',
     component: SecretComponent,
-  }
+    canActivate: [AuthGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
