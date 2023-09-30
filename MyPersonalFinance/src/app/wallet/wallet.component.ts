d22a9922a47b4f3c40530f1a56249c6a10bb7c1f
murilothebr <murilothebr@hotmail.com>
2023-09-30 18:48:24 -0300

    added new client for user wallet operations

--- MyPersonalFinance/src/app/wallet/wallet.component.ts
+++ MyPersonalFinance/src/app/wallet/wallet.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WalletClient } from '../clients/wallet.client';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-wallet',
@@ -6,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wallet.component.css']
 })
 export class WalletComponent implements OnInit {
+  public userWallets: Observable<any> = this.walletClient.getUserWallets();
 
-  constructor() { }
+  constructor(
+    private walletClient: WalletClient
+    ) {}
 
   ngOnInit(): void {
   }
--- MyPersonalFinance/src/environments/environment.ts
+++ MyPersonalFinance/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:62315',
+  apiUrl: 'https://localhost:32768',
+  apiRouteWallet: 'https://localhost:32768/api/Wallet/GetUserWallets?userId='
 };
