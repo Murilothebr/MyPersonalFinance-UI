b2de5d3b5cd8e06b2cf8343e6da7b35eb62582ea
murilothebr <murilothebr@hotmail.com>
2023-10-07 17:53:03 -0300

    header files

--- MyPersonalFinance/src/app/header/header.component.ts
+++ MyPersonalFinance/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  @Output() logoutEvent = new EventEmitter<void>();
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  logout(): void {
+    this.authenticationService.logout();
+  }
 }
